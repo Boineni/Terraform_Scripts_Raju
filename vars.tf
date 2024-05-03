f7f53a57597f083dbd114bbd6d78efe98b278fbd
Vishnuvardhan <61036801+nvvr53@users.noreply.github.com>
2024-05-03 08:23:24 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "region" {
+    default = "ap-south-2"
+}
+
+variable "access_key" {
+    default = "AKIA3FLDXZMVFL5AXNFL"
+}
+
+variable "secret_key" {
+    default = "EGb1RLSWKa7pY4n9Ds41bffTPliN0VDvuEOhDoss"
+}
+
+variable "cluster_name" {
+    default = "nvvr-cluster"
+}
+
+variable "iam_role" {
+    default = "eks-role"
+}
+
+variable "vpc" {
+    default = "nvvr-vpc"
+}
